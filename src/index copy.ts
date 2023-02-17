e82d84c246f0588283c40492b1907b52e3f4c9bb
Charles Berman <Charles.Heath.Berman@Gmail.com>
2023-02-17 16:24:52 -0500

    try publishing again

--- src/index copy.ts
+++
@@ -1,9 +0,0 @@
-
-export { default as Crate } from './Crate';
-export { default as Database } from './Database';
-
-
-export { listCrates, listCratesSync } from './listCrates';
-
-export { default as getDefaultPath } from './util/getDefaultPath';
-
